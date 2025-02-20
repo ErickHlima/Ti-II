e17bb035d0a952e17f9d891905ede2cb2f2b440c
ErickHlima <165850219+ErickHlima@users.noreply.github.com>
2025-02-19 23:42:27 -0300

    Add files via upload

---
+++ Ex1/src/ex1/Ex01.java
@@ -0,0 +1,14 @@
+package ex1;
+import java.util.*;
+public class Ex01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		System.out.println("Soma: " + (n1+n2));
+		sc.close();
+	}
+
+}
